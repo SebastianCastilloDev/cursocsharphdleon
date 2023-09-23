e03020ab9437dcea03b12bcfcf098553761c0550
Sebastian Castillo <117539633+SebastianCastilloDev@users.noreply.github.com>
2023-09-23 19:10:24 +0000

    primer commit

---
+++ inicio/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numeros = new int[5] { 1, 2, 3, 4, 5 }; // Array de 5 elementos
+
+int numero = numeros[0]; // 1
+
+Console.WriteLine(numero);
+
+for (int i=0; i < numeros.Length; i++) {
+    
+}
